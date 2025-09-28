8607b3665edf91c7c7ed32a96f454f8d95be5d1c
Sameer Yasin <113780206+SameerYasin@users.noreply.github.com>
2025-09-29 04:36:21 +0530

    Create Exp-16 Exception Handling in CPP_1.cpp

---
+++ Exp-16 Exception Handling in CPP_1.cpp
@@ -0,0 +1,44 @@
+//Name: Sameer Yasin
+//PRN: 24070123092
+//Branch: ENTC B1
+//Exp-16
+
+//Program-1
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float num1, num2, result;
+
+    cout << "Enter the values of 2 numbers: ";
+    cin >> num1 >> num2;
+
+    try{
+        if(num2==0){
+            throw(num2);
+        }
+
+        else{
+            result = num1/num2;
+            cout << "Result: " << result << endl;
+        }
+    }
+
+    catch(float n){
+        cout << "Exception: Division by zero is not allowed!" << endl;
+    }
+
+    return 0;
+}
+
+
+/*==========OUTPUTS==========
+Enter the values of 2 numbers: 10
+20
+Result: 0.5
+
+Enter the values of 2 numbers: 1
+0
+Exception: Division by zero is not allowed!
+*/
